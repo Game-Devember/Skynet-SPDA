b9c8da47fce884aea39ca587727c2c51bfdb7d5d
Sahil Singh <sahilsingh10394@gmail.com>
2015-03-21 16:30:58 +0530

    Version 1.0.0a
    Working

---
+++ #wehack/Assets/Scripts/watchControl.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var hours : String ;
+var minutes : String;
+var seconds : String;
+
+function Start () 
+{
+
+}
+
+function Update () 
+{
+	Debug.Log(System.DateTime.Now);
+	hours = System.DateTime.Now.ToString("HH");
+	minutes = System.DateTime.Now.ToString("mm");
+	seconds = System.DateTime.Now.ToString("ss");
+}
